149ff0dfd9bc1fb8c7a076129dbae3efdd4bba11
br3athein <br3athein@gmail.com>
2022-06-08 15:23:26 +0300

    add statuses & public count @ Article

--- app/app/models/concerns/visible.rb
+++ app/app/models/concerns/visible.rb
@@ -7,7 +7,24 @@ module Visible
     validates :status, inclusion: { in: VALID_STATUSES }
   end
 
+  class_methods do
+    def visible_count
+      where(status: 'public').count
+    end
+  end
+
   def archived?
     status == 'archived'
+  end
+
+  def status_adjective
+    case status
+    when 'public'
+      'publicly'
+    when 'private'
+      'privately'
+    else
+      'somehow'
+    end
   end
 end
