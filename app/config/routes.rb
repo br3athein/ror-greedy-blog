e2d76fb975633643b92d6e3ba35b3761ca025cb4
br3athein <br3athein@gmail.com>
2022-06-10 19:23:48 +0300

    who needs the blog when we have the greed?

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'articles#index'
+  root 'greed_sessions#index'
 
   resources :articles do
     resources :comments, only: [:create, :destroy]
